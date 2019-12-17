207d03919d2c4e58438735a6bf83ea1e9d1168a0
Mickael GOETZ <contact@mickael-goetz.com>
2019-12-17 18:03:35 +0100

    handle edge-case of january dates being announced in december (with no year)

--- src/scrapper.mjs
+++ src/scrapper.mjs
@@ -2,6 +2,7 @@ import crypto from "crypto";
 import moment, { momentFromTimestamp } from "./moment";
 import puppeteer from "puppeteer";
 import urls from "./urls";
+import Sentry from '@sentry/node';
 
 const md5 = data => crypto.createHash('md5').update(data).digest("hex");
 
@@ -50,6 +51,7 @@ async function scrape(url) {
             const titleContainer = descriptionContainer.querySelector(".title");
             const screeningAnchor = descriptionContainer.querySelector("a");
             const typeContainer = descriptionContainer.querySelector(".period .type");
+            const dateContainer = screeningContainer.querySelector('.date');
 
             // no need to import URL, it exists in the context of the page
             const url = new URL(screeningAnchor.href);
@@ -60,11 +62,18 @@ async function scrape(url) {
             const date = screeningAnchor.innerText.trim();
             const type = typeContainer.innerText.toUpperCase().trim();
 
+            const shortDateParts = dateContainer.innerText.trim().match(/(?<day>\d+)\s+(?<month>\d+)/);
+            let month = null;
+            if (shortDateParts.groups && shortDateParts.groups.month) {
+                month = parseInt(shortDateParts.groups.month);
+            }
+
             return {
                 title,
                 screeningId,
                 cover,
                 date,
+                month,
                 type
             }
         });
@@ -72,8 +81,29 @@ async function scrape(url) {
 
     await browser.close();
 
+    const now = moment();
+    const year = now.year();
+    const month = now.month() + 1; // moment month are 0 based
+
     for (const screening of screenings) {
-        const dateObject = moment(screening.date, "dddd DD MMMM HH:mm", "fr");
+        let dateObject = moment(screening.date, "dddd DD MMMM HH:mm", "fr");
+
+        // Since the year is not included, moment use the current year as default.
+        // When we are in december, some january dates may already have been announced.
+        // For example, on the 17th of december 2019, a screening for the 2nd of january 2020 has been announced.
+        // Moment tries to parse it with the current year and an invalid moment object is produced because the
+        // day doesn't match (02/01/2020 is a thursday and 02/01/2019 was a wednesday).
+        // So if the moment object is invalid and the month of the screening is a month that comes before the current
+        // month, try to re-parse the date using next year.
+        // If that fails, throw an error.
+        if (!dateObject.isValid() && screening.month < month) {
+            dateObject = moment(`${year + 1} ${screening.date}`, "YYYY dddd DD MMMM HH:mm", "fr");
+
+            if (!dateObject.isValid()) {
+                throw new Error(`Unable to parse a valid date for ${JSON.stringify(screening)}`);
+            }
+        }
+
         const date = dateObject.format("dddd DD MMMM [à] HH[h]mm");
         const timestamp = dateObject.unix();
         const id = `${timestamp}-${screening.screeningId}-${md5(screening.title)}`;
